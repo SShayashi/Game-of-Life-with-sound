a977783fe95f5351232e08e2f18d1137898e33f4
hayashi <sshayashi@star-prince.comsshayashi@star-prince.comsshayashi@star-prince.com>
2017-06-24 18:58:40 +0900

    ２次元セルオートマトンのモデルを追加した

---
+++ app/src/main/java/com/sshayashi/musicgame/CellularAutomata.java
@@ -0,0 +1,58 @@
+package com.sshayashi.musicgame;
+
+/**
+ * Created by starprince on 2017/06/24.
+ */
+
+public class CellularAutomata {
+    int[][][] world;
+    int width, height;
+
+    enum Status{
+        a,
+        b,
+        c
+    };
+
+    CellularAutomata(int width, int height){
+        this.width = width;
+        this.height = height;
+        //sx * sy の大きさの配列を2つ準備
+        //ここで、1つめは現在の世代、2つめは次の世代を保管するために使う
+        world = new int[this.width][this.height][2];
+
+
+    }
+
+    void update(){
+
+        for (int x = 0; x < width; x=x+1) {
+            for (int y = 0; y < height; y=y+1) {
+
+                if ((world[x][y][1] == 1) || (world[x][y][1] == 0 && world[x][y][0] == 1))
+                {
+                    world[x][y][0] = 1;
+//                    point(x, y);
+                }
+                if (world[x][y][1] == -1)
+                {
+                    world[x][y][0] = 0;
+                }
+                world[x][y][1] = 0;
+            }
+        }
+    }
+
+    /* 周囲のセルの生死を判定する関数 */
+    int neighbors(int x, int y)
+    {
+        return world[(x + 1) % width][y][0] +
+                world[x][(y + 1) % height][0] +
+                world[(x + width - 1) % width][y][0] +
+                world[x][(y + height - 1) % height][0] +
+                world[(x + 1) % width][(y + 1) % height][0] +
+                world[(x + width - 1) % width][(y + 1) % height][0] +
+                world[(x + width - 1) % width][(y + height - 1) % height][0] +
+                world[(x + 1) % width][(y + height - 1) % height][0];
+    }
+}
